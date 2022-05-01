4ca6cd299221f4e667e438ec7a0779780e1af763
-hasty- <tikhomirovtimofei@gmail.com>
2022-05-01 17:18:59 +0300

    Refactor main to use separate functions for each command

--- run.sh
+++ run.sh
@@ -2,27 +2,24 @@
 
 bin=./target/release/knapsack
 message='tihomirov timofey mihaylovich'
-keyfile=knapsack.keys
 
 printf "message: %s\n\n" "$message"
 
 $bin keys
 printf "\n"
 
-private_key=$(awk 'NR == 1' $keyfile)
-a=$(awk 'NR == 2' $keyfile)
-n=$(awk 'NR == 3' $keyfile)
-public_key=$(awk 'NR == 4' $keyfile)
+private_key=$(awk 'NR == 1' private.key)
+a=$(awk 'NR == 2'  private.key)
+n=$(awk 'NR == 3'  private.key)
+public_key=$(cat public.key)
 
 printf "%12s %s\n" "private key:" "$private_key"
 printf "%12s %d\n" "a:" "$a"
 printf "%12s %d\n" "n:" "$n"
 printf "%12s %s\n\n" "public key:" "$public_key"
 
-encrypted=$($bin encrypt "$keyfile" "$message")
+encrypted=$(echo "$message" | $bin encrypt)
 printf "encrypted: %s\n\n" "$encrypted"
 
-decrypted=$($bin decrypt "$keyfile" $encrypted)
+decrypted=$(echo "$encrypted" | $bin decrypt)
 printf "decrypted: %s\n" "$decrypted"
-
-rm $keyfile
--- src/bin/knapsack.rs
+++ src/bin/knapsack.rs
@@ -1,8 +1,9 @@
 use std::{
+    error::Error,
     fmt::Debug,
     fs::File,
-    io::{BufRead, BufReader, Write},
-    path::PathBuf,
+    io::{self, BufRead, BufReader, Read, Write},
+    path::{Path, PathBuf},
 };
 
 use clap::{Parser, Subcommand};
@@ -18,7 +19,6 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    #[clap(arg_required_else_help = false)]
     /// Generate private and public keys and save them to file
     Keys {
         /// Path to private key file
@@ -29,25 +29,114 @@ enum Commands {
         public_key: PathBuf,
     },
     /// Encrypt given text
-    #[clap(arg_required_else_help = true)]
     Encrypt {
         /// Public key of the recipient
         #[clap(short, long, default_value = "public.key", parse(from_os_str))]
         keyfile: PathBuf,
         /// Message to encrypt
-        #[clap(required = true)]
-        text: String,
+        #[clap(required = false, parse(from_os_str))]
+        textfile: Option<PathBuf>,
     },
     /// Decrypt the cipher
-    #[clap(arg_required_else_help = true)]
     Decrypt {
         /// Private key of the recipient
         #[clap(short, long, default_value = "private.key", parse(from_os_str))]
         keyfile: PathBuf,
         /// Encrypted message
-        #[clap(multiple_values = true, required = true)]
-        cipher: Vec<u32>,
+        #[clap(required = false, parse(from_os_str))]
+        cipherfile: Option<PathBuf>,
     },
+}
+
+fn generate_keys(private_key: &Path, public_key: &Path) -> io::Result<()> {
+    let keys = keys::KeyPair::new(8);
+
+    let mut private_key_file = File::create(&private_key)?;
+    private_key_file.write_fmt(format_args!(
+        "{}\n{}\n{}",
+        keys.private_key()
+            .iter()
+            .map(|x| x.to_string() + " ")
+            .collect::<String>(),
+        keys.a(),
+        keys.n()
+    ))?;
+
+    let mut public_key_file = File::create(&public_key)?;
+
+    public_key_file.write_fmt(format_args!(
+        "{}",
+        keys.public_key()
+            .iter()
+            .map(|x| x.to_string() + " ")
+            .collect::<String>()
+    ))?;
+
+    Ok(())
+}
+
+fn encrypt(keyfile: PathBuf, textfile: Option<PathBuf>) -> Result<String, Box<dyn Error>> {
+    let input = File::open(keyfile)?;
+    let buffered = BufReader::new(input);
+    let public_key = buffered
+        .lines()
+        .last()
+        .ok_or("unable to read public key")??;
+    let public_key = public_key
+        .split_whitespace()
+        .map(|x| x.parse::<u32>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut input: Box<dyn io::Read> = if let Some(textfile) = textfile {
+        Box::new(File::open(textfile)?)
+    } else {
+        Box::new(io::stdin())
+    };
+
+    let mut text = String::new();
+    input.read_to_string(&mut text)?;
+    let encrypted = merkle_hellman_knapsack::encrypt(text.trim(), &public_key);
+
+    Ok(encrypted
+        .iter()
+        .map(|x| x.to_string() + " ")
+        .collect::<String>())
+}
+
+fn decrypt(keyfile: PathBuf, cipherfile: Option<PathBuf>) -> Result<String, Box<dyn Error>> {
+    let input = File::open(keyfile)?;
+    let mut lines = BufReader::new(input).lines();
+    let private_key = lines
+        .next()
+        .ok_or("unable to read private key")??
+        .split_whitespace()
+        .map(|x| x.parse::<u32>())
+        .collect::<Result<Vec<_>, _>>()?;
+    let a = lines
+        .next()
+        .ok_or("invalid parameter 'a'")??
+        .parse::<u32>()?;
+    let n = lines
+        .next()
+        .ok_or("invalid parameter 'n'")??
+        .parse::<u32>()?;
+
+    let mut input: Box<dyn io::Read> = if let Some(cipherfile) = cipherfile {
+        Box::new(File::open(cipherfile)?)
+    } else {
+        Box::new(io::stdin())
+    };
+
+    let mut text = String::new();
+    input.read_to_string(&mut text)?;
+
+    let cipher = text
+        .split_whitespace()
+        .map(|x| x.parse::<u32>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let decrypted = merkle_hellman_knapsack::decrypt(&cipher, &private_key, a, n);
+    Ok(decrypted)
 }
 
 fn main() {
@@ -57,85 +146,33 @@ fn main() {
         Commands::Keys {
             private_key,
             public_key,
-        } => {
-            let keys = keys::KeyPair::new(8);
-
-            let mut private_key_file =
-                File::create(&private_key).expect("unable to create file {private_key}");
-            private_key_file
-                .write_fmt(format_args!(
-                    "{}\n{}\n{}",
-                    keys.private_key()
-                        .iter()
-                        .map(|x| x.to_string() + " ")
-                        .collect::<String>(),
-                    keys.a(),
-                    keys.n()
-                ))
-                .expect("unable to write private key to {private_key}");
-            println!("saved private key to {}", private_key.to_str().unwrap(),);
-
-            let mut public_key_file =
-                File::create(&public_key).expect("unable to create file {public_key}");
-
-            public_key_file
-                .write_fmt(format_args!(
-                    "{}",
-                    keys.public_key()
-                        .iter()
-                        .map(|x| x.to_string() + " ")
-                        .collect::<String>()
-                ))
-                .expect("unable to write public key to {public_key}");
-            println!("saved public key to {}", public_key.to_str().unwrap(),);
-        }
-        Commands::Encrypt { keyfile, text } => {
-            let input = File::open(keyfile).expect("invalid path to keyfile");
-            let buffered = BufReader::new(input);
-            let public_key = buffered
-                .lines()
-                .last()
-                .expect("missing public key")
-                .unwrap();
-            let public_key = public_key
-                .split_whitespace()
-                .map(|x| x.parse::<u32>().expect("invalid public key"))
-                .collect::<Vec<u32>>();
-            let encrypted = merkle_hellman_knapsack::encrypt(&text, &public_key);
-
-            println!(
-                "{}",
-                encrypted
-                    .iter()
-                    .map(|x| x.to_string() + " ")
-                    .collect::<String>()
-            );
-        }
-        Commands::Decrypt { keyfile, cipher } => {
-            let input = File::open(keyfile).expect("invalid path to keyfile");
-            let mut lines = BufReader::new(input).lines();
-            let private_key = lines
-                .next()
-                .expect("missing private key")
-                .unwrap()
-                .split_whitespace()
-                .map(|x| x.parse::<u32>().expect("invalid private key"))
-                .collect::<Vec<u32>>();
-            let a = lines
-                .next()
-                .expect("missing parameter 'a'")
-                .unwrap()
-                .parse::<u32>()
-                .expect("invalid parameter 'a'");
-            let n = lines
-                .next()
-                .expect("missing parameter 'n'")
-                .unwrap()
-                .parse::<u32>()
-                .expect("invalid parameter 'n'");
-            let decrypted = merkle_hellman_knapsack::decrypt(&cipher, &private_key, a, n);
-
-            println!("{decrypted}");
-        }
+        } => match generate_keys(&private_key, &public_key) {
+            Ok(_) => {
+                println!("saved private key to {}", private_key.to_str().unwrap());
+                println!("saved public key to {}", public_key.to_str().unwrap());
+            }
+            Err(error) => {
+                eprintln!("{}", error);
+            }
+        },
+        Commands::Encrypt { keyfile, textfile } => match encrypt(keyfile, textfile) {
+            Ok(cipher) => {
+                println!("{}", cipher);
+            }
+            Err(error) => {
+                eprintln!("{}", error);
+            }
+        },
+        Commands::Decrypt {
+            keyfile,
+            cipherfile,
+        } => match decrypt(keyfile, cipherfile) {
+            Ok(decrypted) => {
+                println!("{}", decrypted);
+            }
+            Err(error) => {
+                eprintln!("{}", error);
+            }
+        },
     }
 }
